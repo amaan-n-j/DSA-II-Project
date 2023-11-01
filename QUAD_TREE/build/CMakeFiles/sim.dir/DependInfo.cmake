c5915423e817d16cbd1d9519b3ee65c54cbdad0a
Anvay Joshi <anvayjoshi1410@gmail.com>
2023-11-01 09:49:43 +0530

    added CMakeLists.txt

---
+++ QUAD_TREE/build/CMakeFiles/sim.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/anvay/COEP/projects/DSA-II-Project/QUAD_TREE/src/particle.c" "CMakeFiles/sim.dir/src/particle.c.o" "gcc" "CMakeFiles/sim.dir/src/particle.c.o.d"
+  "/home/anvay/COEP/projects/DSA-II-Project/QUAD_TREE/src/qt_main.c" "CMakeFiles/sim.dir/src/qt_main.c.o" "gcc" "CMakeFiles/sim.dir/src/qt_main.c.o.d"
+  "/home/anvay/COEP/projects/DSA-II-Project/QUAD_TREE/src/randomParticleGenerator.c" "CMakeFiles/sim.dir/src/randomParticleGenerator.c.o" "gcc" "CMakeFiles/sim.dir/src/randomParticleGenerator.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ QUAD_TREE/build/CMakeFiles/sim.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sim.dir/src/particle.c.o"
+  "CMakeFiles/sim.dir/src/particle.c.o.d"
+  "CMakeFiles/sim.dir/src/qt_main.c.o"
+  "CMakeFiles/sim.dir/src/qt_main.c.o.d"
+  "CMakeFiles/sim.dir/src/randomParticleGenerator.c.o"
+  "CMakeFiles/sim.dir/src/randomParticleGenerator.c.o.d"
+  "sim"
+  "sim.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/sim.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
